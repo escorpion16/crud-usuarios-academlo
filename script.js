7844ea8fc8899f3dca8734aa2054459e7601c84b
dennisx <denizponce16@gmail.com>
2020-06-17 06:07:47 -0500

    commit inicial

---
+++ script.js
@@ -0,0 +1,14 @@
+
+// 1.- Obtener el elemento que quiero insertar en HTML
+// 2.- Generar la estructura de HTML
+// 3.- Insertar el HTML generado
+
+
+function createRow(){
+    return newRow = `<tr>
+                        <th scope="row"></th>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>`
+}
