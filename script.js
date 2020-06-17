8c59c4b6ff9161e46ee7806bd2665e4f22da9fcb
dennisx <denizponce16@gmail.com>
2020-06-17 18:16:43 -0500

    SE ha comentado el codigo y se ha creado la funcion printUser

--- script.js
+++ script.js
@@ -4,45 +4,57 @@ import {usersAcademlo} from './users.js'
 // 3.- Insertar el HTML generado
 
 
-function createRow(){
-    const tableBody = document.querySelector('tbody')
+// function createRow(){
+//     const tableBody = document.querySelector('tbody')
 
-    for(let i = 0; i< usersAcademlo.length ; i++){
-        usersAcademlo[i].index = i +1
-        tableBody.innerHTML += returnHTMLRow(usersAcademlo[i])
-    }
+//     for(let i = 0; i< usersAcademlo.length ; i++){
+//         usersAcademlo[i].index = i +1
+//         tableBody.innerHTML += returnHTMLRow(usersAcademlo[i])
+//     }
     
-}
+// }
 
-function returnHTMLRow(user){
-    return `<tr>
-                <th scope="row">${user.index}</th>
-                <td>${user.name}</td>
-                <td>${user.lastname}</td>
-                <td>${user.email}</td>
-            </tr>`
-}
+// function returnHTMLRow(user){
+//     return `<tr>
+//                 <th scope="row">${user.index}</th>
+//                 <td>${user.name}</td>
+//                 <td>${user.lastname}</td>
+//                 <td>${user.email}</td>
+//             </tr>`
+// }
 
-function addUser(){
-    const name =  document.querySelector('#name').value
-    const lastname = document.querySelector('#lastname').value
-    const email =  document.querySelector('#email').value
+// function addUser(){
+//     const name =  document.querySelector('#name').value
+//     const lastname = document.querySelector('#lastname').value
+//     const email =  document.querySelector('#email').value
 
-    const totalUsers = usersAcademlo.length
-    const newUser = {
-        index: totalUsers,
-        name: name,
-        lastname: lastname,
-        email: email
-    }
-    usersAcademlo.unshift(newUser)
-    console.log(usersAcademlo)
+//     const totalUsers = usersAcademlo.length
+//     const newUser = {
+//         index: totalUsers,
+//         name: name,
+//         lastname: lastname,
+//         email: email
+//     }
+//     usersAcademlo.unshift(newUser)
+//     console.log(usersAcademlo)
+//     const tableBody = document.querySelector('tbody')
+//     const allUsers = returnHTMLRow(newUser) + tableBody.innerHTML
+//     tableBody.innerHTML = allUsers
+
+
+// }
+
+function printUser(){
     const tableBody = document.querySelector('tbody')
-    const allUsers = returnHTMLRow(newUser) + tableBody.innerHTML
-    tableBody.innerHTML = allUsers
-
-
+    usersAcademlo.forEach((user,index) => {
+        tableBody.innerHTML += `<tr>
+                                    <th scope="row">${index +1}</th>
+                                    <td>${user.name}</td>
+                                    <td>${user.lastname}</td>
+                                    <td>${user.email}</td>
+                                </tr>`
+    })
 }
 
-createRow()
-window.addUser = addUser
+printUser()
+// window.addUser = addUser
